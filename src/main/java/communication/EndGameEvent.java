d5d48a9c4f912b8dd31d7db06fb91f06a99aa71e
Karl 'NaN' Wikström <karlwik@student.chalmers.se>
2018-02-01 00:31:18 +0100

    Add EndGameEvent class

---
+++ src/main/java/communication/EndGameEvent.java
@@ -0,0 +1,11 @@
+package communication;
+
+/**
+ * Represents a end_game event sent from the server and is used to convert the json received into a Java class which
+ * can be easily used by the program.
+ */
+public class EndGameEvent {
+
+    private String eventName;
+
+}
