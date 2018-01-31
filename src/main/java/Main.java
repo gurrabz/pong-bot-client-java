71b4aa0604084a6cac048572b28adf2d06e8a172
Karl 'NaN' Wikström <karlwik@student.chalmers.se>
2018-01-31 23:48:05 +0100

    Change indentation to tabs
    Signed-off-by: Karl 'NaN' Wikström <karlwik@student.chalmers.se>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,32 +5,32 @@ import java.net.UnknownHostException;
 
 public class Main {
 
-    private final static String    HOST    = "10.0.0.170";
-    private final static int       PORT    = 4242;
-	private final static String    NAME    = "EpicPlayer1337";
-	private final static Player    PLAYER  = new Bot();
+	private final static String     HOST    = "localhost";
+	private final static int        PORT    = 4242;
+	private final static String     NAME    = "EpicPlayer1337";
+	private final static Player     PLAYER  = new Bot();
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Store store = new Store();
-        Communicator communicator = null;
-        try {
-            communicator = new Communicator(store, PORT, InetAddress.getByName(HOST),
-                    NAME);
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("Couldn't create Socket...");
-            e.printStackTrace();
-            System.exit(1);
-        }
+		Store store = new Store();
+		Communicator communicator = null;
+		try {
+			communicator = new Communicator(store, PORT, InetAddress.getLocalHost(), NAME); // Debug
+			//communicator = new Communicator(store, PORT, InetAddress.getByName(HOST), NAME);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (IOException e) {
+			System.err.println("Couldn't create Socket...");
+			e.printStackTrace();
+			System.exit(1);
+		}
 
-        communicator.start();
+		communicator.start();
 
-        while (true) {
-        	store.setDesiredPaddleState(PLAYER.play(store.getGameState()));
-        }
+		while (true) {
+				store.setDesiredPaddleState(PLAYER.play(store.getGameState()));
+		}
 
-    }
+	}
 }
