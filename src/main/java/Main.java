cf316089469ec435f793cc6fde788901edc13b4c
Karl 'NaN' Wikström <karlwik@student.chalmers.se>
2018-02-02 16:22:03 +0100

    Setup Main.java in preperation for event

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,20 +5,17 @@ import java.net.UnknownHostException;
 
 public class Main {
 
-	private final static String     HOST    = "localhost";
+	private final static String     HOST    = "10.0.0.170";
 	private final static int        PORT    = 4242;
 	private final static String     NAME    = "EpicPlayer1337";
 	private final static Player     PLAYER  = new Bot();
-
-	public static Boolean   IN_GAME = false; // TODO: this variable doesn't synchronize across threads...
 
 	public static void main(String[] args) {
 
 		Store store = new Store();
 		Communicator communicator = null;
 		try {
-			communicator = new Communicator(store, PORT, InetAddress.getLocalHost(), NAME); // Debug
-			//communicator = new Communicator(store, PORT, InetAddress.getByName(HOST), NAME);
+			communicator = new Communicator(store, PORT, InetAddress.getByName(HOST), NAME);
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 			System.exit(1);
@@ -31,9 +28,7 @@ public class Main {
 		communicator.start();
 
 		while (true) {
-			if(IN_GAME) {
-				store.setDesiredPaddleState(PLAYER.play(store.getGameState()));
-			}
+			store.setDesiredPaddleState(PLAYER.play(store.getGameState()));
 		}
 
 	}
